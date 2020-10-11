2c3ebd24a07137225a725d6979dac9027f3ac1d8
Naeun <knae11@naver.com>
2020-10-11 16:21:00 +0900

    calender which weekday?

--- src/naeun/calender/Calender.java
+++ src/naeun/calender/Calender.java
@@ -1,41 +1,52 @@
 package naeun.calender;
 
-import java.util.Scanner;
-
 public class Calender {
 
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final String[] WEEKDAYS = { "SU", "MO", "TU", "WE", "TH", "FR", "SA" };
 
-	public int maxDaysOfMonth(int month) {
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public int maxDaysOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month - 1];
+		}
 		return MAX_DAYS[month - 1];
 	}
 
-	public void printSampleOfMonth() {
-		System.out.println("월  화  수  목  금  토  일\r\n" + "--------------------\r\n" + " 1  2  3  4  5  6  7\r\n"
-				+ " 8  9 10 11 12 13 14\r\n" + "15 16 17 18 19 20 21\r\n" + "22 23 24 25 26 27 28");
-	}
-
-	public static void main(String[] args) {
-		String PROMPT = "cal> ";
-		Scanner scanner = new Scanner(System.in);
-		Calender cal = new Calender();
-		while(true) {
-			System.out.println("달을 입력하세요");
-			System.out.print(PROMPT);
-			int month = scanner.nextInt();
-			if(month <1 ) {
-				break;
-			}
-			if(month>12) {
-				continue;
-			}
-			System.out.printf("%d의 마지막날은 %d입니다.\n", month, cal.maxDaysOfMonth(month));
-
+	public void printCalender(int year, int month, int weekday) {
+		System.out.printf("   <<%4d년%3d월>>\n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA\r\n" + "----------------------");
+		// print blank space
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
 		}
 
-		System.out.println("끝");
+		int maxDay = maxDaysOfMonth(year, month);
+		int count = 7 - weekday;
+		int delim = count<7? count : 0;
 
-		scanner.close();
+		// print first line
+		for (int i = 1; i <= count; i++) {
+			System.out.printf("%3d", i);
+		}
+		System.out.println();
 
+		for (int j = count + 1; j <= maxDay; j++) {
+			System.out.printf("%3d", j);
+			if (j % 7 == delim) {
+				System.out.println();
+			}
+		}
+		System.out.println();
+		System.out.println();
 	}
+
 }
---
+++ src/naeun/calender/Prompt.java
@@ -0,0 +1,75 @@
+package naeun.calender;
+
+import java.util.Scanner;
+
+public class Prompt {
+	/*
+	 * @param week 요일명
+	 * 
+	 * @return 0~6 (0=Sunday~6=Saturday)
+	 */
+	public int parseDay(String week) {
+		if (week.equals("su"))
+			return 0;
+		else if (week.equals("mo"))
+			return 1;
+		else if (week.equals("tu"))
+			return 2;
+		else if (week.equals("we"))
+			return 3;
+		else if (week.equals("th"))
+			return 4;
+		else if (week.equals("fr"))
+			return 5;
+		else if (week.equals("sa"))
+			return 6;
+		else
+			return 0;
+
+	}
+
+	public void runPrompt() {
+		Scanner scanner = new Scanner(System.in);
+		Calender cal = new Calender();
+
+		int month = 1;
+		int year = 2017;
+		int weekday = 0;
+
+		while (true) {
+			System.out.println("년을 입력하세요");
+			System.out.print("YEAR> ");
+			year = scanner.nextInt();
+			if (year == -1) {
+				break;
+			}
+			System.out.println("달을 입력하세요");
+			System.out.print("MONTH> ");
+			month = scanner.nextInt();
+			if (month > 12 || month < 1) {
+				System.out.println("잘못된 입력 입니다.");
+				continue;
+			}
+			System.out.println("첫째 날의 요일을 입력하세요 : su, mo, tu, we, th, fr, sa");
+			System.out.print("WEEKDAY> ");
+			String str_weekday = scanner.next();
+			weekday = parseDay(str_weekday);
+
+			
+			cal.printCalender(year, month, weekday);
+
+		}
+
+		System.out.println("끝");
+
+		scanner.close();
+
+	}
+
+	public static void main(String[] args) {
+
+		Prompt p = new Prompt();
+		p.runPrompt();
+
+	}
+}
